7fcf7c93d70b7ae34d03e90f0b56bceb893ba433
IndiartoAji13 <indiartoaji13@gmail.com>
2024-02-01 15:42:51 +0700

    Add period in image path

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
           <div className="rounded-full bg-[#64A9A5] w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] relative md:w-[200px] md:h-[200px] mr-10">
             <Image
               src="./images/dp_image.png"
-              alt="hero image"
+              alt="dp image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
