8f98f2bfc7e4c778fbbe69d156b302b855bc57c7
IndiartoAji13 <indiartoaji13@gmail.com>
2024-02-01 15:56:07 +0700

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import dpimage from "./images/dp-image.png";
 
 const HeroSection = () => {
   return (
@@ -18,8 +19,8 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#64A9A5] w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] relative md:w-[200px] md:h-[200px] mr-10">
             <Image
-              src="images/dp-image.png"
-              alt="images/  dp image"
+              src={dpimage}
+              alt="./images/  dp image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
